ce04c7fd1fd97b010f7051dd7270cf75c20d1301
anankhongngu <63190200+anankhongngu@users.noreply.github.com>
2025-12-24 15:01:27 +0700

    Update Theme.vue Target Blank

--- src/components/Theme/Theme.vue
+++ src/components/Theme/Theme.vue
@@ -2,39 +2,39 @@
     <div class="w3-row-padding">
             <div class="w3-third w3-container w3-margin-bottom ">
                 <div class="w3-image">
-                    <a href="https://doraemon-store.myharavan.com/">
+                    <a target="_blannk" href="https://doraemon-store.myharavan.com/">
                         <img src="./../../assets/image/product/dora_store.png" alt="Teddy" class="w3-hover-opacity" style="width:100%">
                     </a>
                 </div>
                 <div class="w3-container w3-white">
                     <p>
-                        <a href="https://doraemon-store.myharavan.com/"><b>Doraemon Store</b></a>
+                        <a target="_blannk" href="https://doraemon-store.myharavan.com/"><b>Doraemon Store</b></a>
                     </p> 
                     <p>Trang mua sắm trực tuyến của thương hiệu thời trang Doraemon...</p>
                 </div>
             </div>
             <div class="w3-third w3-container w3-margin-bottom ">
                 <div class="w3-image">
-                    <a href="https://doraemon-cosmetics.myharavan.com/">
+                    <a target="_blannk" href="https://doraemon-cosmetics.myharavan.com/">
                         <img src="./../../assets/image/product/dora_cosmetic.png" alt="Teddy" class="w3-hover-opacity" style="width:100%">
                     </a>
                 </div>
                 <div class="w3-container w3-white">
                     <p>
-                        <a href="https://doraemon-cosmetics.myharavan.com/"><b>Doraemon Cosmetic</b></a>
+                        <a target="_blannk" href="https://doraemon-cosmetics.myharavan.com/"><b>Doraemon Cosmetic</b></a>
                     </p> 
                     <p>Theme mua sắm, làm đẹp của thương hiệu Doraemon. Với đầy đủ các tính năng phù hợp cho mọi cửa hàng như: mỹ phẩm, thời trang, quảng bá sản phẩm,...</p>
                 </div>
             </div>
             <div class="w3-third w3-container w3-margin-bottom ">
                 <div class="w3-image">
-                    <a href="https://doraemon-furniture.myharavan.com/">
+                    <a target="_blannk" href="https://doraemon-furniture.myharavan.com/">
                         <img src="./../../assets/image/product/dora_furniture.png" alt="Teddy" class="w3-hover-opacity" style="width:100%">
                     </a>
                 </div>
                 <div class="w3-container w3-white">
                     <p>
-                        <a href="https://doraemon-furniture.myharavan.com/"><b>Doraemon Furniture</b></a>
+                        <a target="_blannk" href="https://doraemon-furniture.myharavan.com/"><b>Doraemon Furniture</b></a>
                     </p> 
                     <p>Theme mua sắm trực tuyến của thương hiệu Doraemon. Với đầy đủ các tính năng phù hợp cho mọi cửa hàng như: nội thất, thiết kế, quảng bá sản phẩm,...</p>
                 </div>
